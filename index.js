5c87535b75d8080b90343dbd3f746b915374a8a2
itscopebd <itscopebd@gmail.com>
2023-05-03 20:18:48 +0600

    api created chefs details

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express=require("express");
+var cors = require('cors')
+
+const allData=require("./allData/data.json");
 const app=express();
+app.use(cors());
 const port=5000;
 
-app.get("/",(req,res)=>{
-    res.send("Rofiq")
+app.get("/alldata",(req,res)=>{
+    res.send(allData)
 })
 
+
+app.get("/alldata/:id",(req,res)=>{
+    const id=req.params.id;
+    const selectedData=allData.find(n=>n.id===id);
+    res.send(selectedData)
+})
 
 
 
